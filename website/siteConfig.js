7c67bdc07d8d24e4c27b934d37dc3d497998af1a
codyfet <codyfet@mail.ru>
2019-04-29 15:29:08 +0300

    Правки страниц

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,18 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'Elevator saga practice course', // Title for your website.
+  tagline: 'Elevator saga practice course',
+  // url: 'https://github.com/codyfet/elevator-saga-practice', // Your website URL
+  url: 'https://codyfet.github.io', // Your website URL
+  baseUrl: '/elevator-saga-practice/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'elevator-saga-practice',
+  organizationName: 'codyfet',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
