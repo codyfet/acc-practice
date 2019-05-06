1705de68c0a77e260eb4ac31c47bf8392ad59a06
codyfet <codyfet@mail.ru>
2019-05-06 15:37:55 +0300

    Добавлены страницы учебника

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -13,223 +13,151 @@ const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
+/**
+ * Компонент "главный блок", сразу под шапкой.
+ */
 class HomeSplash extends React.Component {
-  render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+    render() {
+        const { siteConfig, language = '' } = this.props;
+        const { baseUrl, docsUrl } = siteConfig;
+        const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+        const langPart = `${language ? `${language}/` : ''}`;
 
-    const SplashContainer = props => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
-        </div>
-      </div>
-    );
+        const SplashContainer = (props) => (
+            <div className="homeContainer">
+                <div className="homeSplashFade">
+                    <div className="wrapper homeWrapper">{props.children}</div>
+                </div>
+            </div>
+        );
 
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
+        /**
+         * Блок с логотипом под шапкой.
+         */
+        const MainBlock = (props) => {
+            return (
+                <div className="main-block-wrapper">
+                    <div className="main-block-text">
+                        <p>Практика в тверском офисе компании <span className="acc-name">Accenture</span></p>
+                    </div>
+                    <div className="main-block-logo">
+                        <img alt="Accenture Logo" className="acc-logo" src={`${baseUrl}img/Accenture-research-medium-marquee.png`}></img>
+                    </div>
+                </div>
 
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
-      </h2>
-    );
+                // <div class="index-hero">
+                //     <div class="index-hero-inner">
+                //         <h1 class="index-hero-project-tagline"> <span class="index-hero-project-keywords">Open Source</span> documentation websites.</h1>
+                //     </div>
+                // </div>
+            );
+        }
 
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
-    );
-
-    return (
-      <SplashContainer>
-        {/* <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} /> */}
-        {/* <img
-            className="adaptive-img lazy  "
-            data-imagexs="//www.accenture.com/t00010101T000000Z__w__/ru-ru/_acnmedia/Accenture/Conversion-Assets/MainPages/Images/Global_26/Accenture-Electric-Purple-Logo-marquee.png"
-            data-imagesm="//www.accenture.com/t00010101T000000Z__w__/ru-ru/_acnmedia/Accenture/Conversion-Assets/MainPages/Images/Global_26/Accenture-Electric-Purple-Logo-marquee.png"
-            data-imagelg="//www.accenture.com/t00010101T000000Z__w__/ru-ru/_acnmedia/Accenture/Conversion-Assets/MainPages/Images/Global_26/Accenture-Electric-Purple-Logo-marquee.png"
-            alt=""
-            src="//www.accenture.com/t00010101T000000Z__w__/ru-ru/_acnmedia/Accenture/Conversion-Assets/MainPages/Images/Global_26/Accenture-Electric-Purple-Logo-marquee.png"
-            style={{display: "block"}}
-            data-src="//www.accenture.com/t00010101T000000Z__w__/ru-ru/_acnmedia/Accenture/Conversion-Assets/MainPages/Images/Global_26/Accenture-Electric-Purple-Logo-marquee.png">
-          </img> */}
-        {/* <Logo img_src={"//www.accenture.com/t00010101T000000Z__w__/ru-ru/_acnmedia/Accenture/Conversion-Assets/MainPages/Images/Global_26/Accenture-Electric-Purple-Logo-marquee.png"} /> */}
-        <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
-          {/* <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection> */}
-          <div>13.05.2019 - 13.06.2019</div>
-        </div>
-      </SplashContainer>
-    );
-  }
+        return (
+            <SplashContainer>
+                <MainBlock />
+            </SplashContainer>
+        );
+    }
 }
 
 class Index extends React.Component {
-  render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
+    render() {
+        const { config: siteConfig, language = '' } = this.props;
+        const { baseUrl } = siteConfig;
 
-    const Block = props => (
-      <Container
-        padding={['bottom', 'top']}
-        id={props.id}
-        background={props.background}>
-        <GridBlock
-          align="center"
-          contents={props.children}
-          layout={props.layout}
-        />
-      </Container>
-    );
+        const Block = props => (
+            <Container
+                padding={['bottom', 'top']}
+                id={props.id}
+                background={props.background}>
+                <GridBlock
+                    align="center"
+                    contents={props.children}
+                    layout={props.layout}
+                />
+            </Container>
+        );
 
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        {/* <h2>Практика включает</h2> */}
-        {/* <MarkdownBlock>Практика включает</MarkdownBlock> */}
-      </div>
-    );
+        /**
+         * Презентация о компании.
+         */
+        const DemoBlock = () => (
+            <Block background="" >
+                {[
+                    {
+                        content: 'Вы узнаете всё, что хотели знать о компании и о том каково это работать в тверском офисе Accenture',
+                        image: `${baseUrl}img/undraw_business_plan_5i9d.svg`,
+                        imageAlign: 'left',
+                        title: 'Презентация о компании',
+                    },
+                ]}
+            </Block>
+        );
 
-    const DemoBlock = () => (
-      <Block  background="light" id="try">
-        {[
-          {
-            content: 'Вы узнаете всё, что хотели знать о компании и о том каково это работать в тверском офисе Accenture',
-            image: `${baseUrl}img/undraw_business_plan_5i9d.svg`,
-            imageAlign: 'left',
-            title: 'Презентация о компании',
-          },
-        ]}
-      </Block>
-    );
+        /**
+         * Программирование на языке JavaScript.
+         */
+        const JavaScriptBlock = () => (
+            <Block background="light">
+                {[
+                    {
+                        content:
+                            'Вы изучите основы программирования и сможете применить полученные знания для решения задач, представленных в игровой форме.',
+                        image: `${baseUrl}img/undraw_programming_2svr.svg`,
+                        imageAlign: 'right',
+                        title: 'Программирование на языке JavaScript',
+                    },
+                ]}
+            </Block>
+        );
 
-    const JavaScriptBlock = () => (
-      <Block>
-        {[
-          {
-            content:
-              'Вы изучите основы программирования и сможете применить полученные знания для решения задач, представленных в игровой форме.',
-            image: `${baseUrl}img/undraw_programming_2svr.svg`,
-            imageAlign: 'right',
-            title: 'Программирование на языке JavaScript',
-          },
-        ]}
-      </Block>
-    );
+        /**
+         * Ещё какой то блок от Лёши.
+         */
+        const AnotherOneBlock = () => (
+            <Block background="">
+                {[
+                    {
+                        content:
+                            'Вы изучите ещё какие то вещи.',
+                        image: `${baseUrl}img/undraw_programming_2svr.svg`,
+                        imageAlign: 'left',
+                        title: 'Ещё какой то блок от Лёши',
+                    },
+                ]}
+            </Block>
+        );
 
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'Вам предстоит примерить на себя роль аналитика и в игровой форме спроектировать прототип приложения',
-            image: `${baseUrl}img/undraw_business_analytics_l92d.svg`,
-            imageAlign: 'right',
-            title: 'Поучаствуете в бизнес-игре',
-          },
-        ]}
-      </Block>
-    );
+        /**
+         * Бизнес-игра.
+         */
+        const BusinessGameBlock = () => (
+            <Block background="dark">
+                {[
+                    {
+                        content:
+                            'Вам предстоит примерить на себя роль аналитика и в игровой форме спроектировать прототип приложения',
+                        image: `${baseUrl}img/undraw_business_analytics_l92d.svg`,
+                        imageAlign: 'right',
+                        title: 'Бизнес-игра',
+                    },
+                ]}
+            </Block>
+        );
 
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content:
-              'Вы изучите основы программирования и сможете применить полученные знания для решения задач, представленных в игровой форме.',
-            image: `${baseUrl}img/undraw_programming_2svr.svg`,
-            imageAlign: 'right',
-            title: 'Программирование на языке JavaScript',
-          },
-        ]}
-      </Block>
-    );
-
-    const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/undraw_react.svg`,
-            imageAlign: 'top',
-            title: 'Feature One',
-          },
-          {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/undraw_operating_system.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two',
-          },
-        ]}
-      </Block>
-    );
-
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    };
-
-    return (
-      <div>
-        <HomeSplash siteConfig={siteConfig} language={language} />
-        <div className="mainContainer">
-          {/* <Features /> */}
-          <FeatureCallout />
-          <DemoBlock />
-          <JavaScriptBlock />
-          <Description />
-          {/* <Showcase /> */}
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <HomeSplash siteConfig={siteConfig} language={language} />
+                <div className="mainContainer">
+                    <DemoBlock />
+                    <JavaScriptBlock />
+                    <AnotherOneBlock />
+                    <BusinessGameBlock />
+                </div>
+            </div>
+        );
+    }
 }
 
 module.exports = Index;
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,97 +10,95 @@
 
 // List of projects/orgs using your project for the users page.
 const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    {
+        caption: 'User1',
+        // You will need to prepend the image path with your baseUrl
+        // if it is not '/', like: '/test-site/img/image.jpg'.
+        image: '/img/undraw_open_source.svg',
+        infoLink: 'https://www.facebook.com',
+        pinned: true,
+    },
 ];
 
 const siteConfig = {
-  title: 'Практика в accenture', // Title for your website.
-  tagline: 'Твой билет в счастливую жизнь!',
-  // url: 'https://github.com/codyfet/acc-practice', // Your website URL
-  url: 'https://codyfet.github.io', // Your website URL
-  baseUrl: '/acc-practice/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+    title: '', // Title for your website.
+    tagline: 'Твой билет в счастливую жизнь!',
+    // url: 'https://github.com/codyfet/acc-practice', // Your website URL
+    url: 'https://codyfet.github.io', // Your website URL
+    baseUrl: '/acc-practice/', // Base URL for your project */
+    // For github.io type URLs, you would set the url and baseUrl like:
+    //   url: 'https://facebook.github.io',
+    //   baseUrl: '/test-site/',
 
-  // Used for publishing and more
-  projectName: 'acc-practice',
-  organizationName: 'codyfet',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+    // Used for publishing and more
+    projectName: 'acc-practice',
+    organizationName: 'codyfet',
+    // For top-level user or org sites, the organization is still the same.
+    // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+    //   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'Переменные', label: 'Материалы'},
-    {doc: 'Переменные', label: 'Преподаватели'}
-  ],
-
-  // If you have users set above, you add it here:
-  users,
-
-  /* path to images for header/footer */
-  headerIcon: 'img/Acc_GT_Dimensional_Purple_RGB_REV.svg',
-  footerIcon: 'img/Acc_GT_Dimensional_Purple_RGB_REV.svg',
-  favicon: 'img/acc_favicon.ico',
-
-  /* Colors for website */
-  colors: {
-    // primaryColor: '#8a3509',
-    primaryColor: '#00D700',
-    secondaryColor: '#A100FF',
-  },
-
-  /* Custom fonts for website */
-    fonts: {
-    myFont: [
-      "Graphik Black",
-      "Arial Black",
-      "Sans-Serif"
+    // For no header links in the top nav bar -> headerLinks: [],
+    headerLinks: [
+        { doc: 'Переменные', label: 'Материалы' },
+        { doc: 'Переменные', label: 'Преподаватели' }
     ],
-    // myOtherFont: [
-    //   "-apple-system",
-    //   "system-ui"
-    // ]
-  },
+
+    // If you have users set above, you add it here:
+    users,
+
+    /* path to images for header/footer */
+    headerIcon: 'img/Acc_GT_Dimensional_Purple_RGB_REV.svg',
+    footerIcon: 'img/Acc_GT_Dimensional_Purple_RGB_REV.svg',
+    favicon: 'img/acc_favicon.ico',
+
+    /* Colors for website */
+    colors: {
+        // primaryColor: '#8a3509',
+        primaryColor: '#00D700',
+        secondaryColor: '#A100FF',
+    },
+
+    /* Custom fonts for website */
+    fonts: {
+        myFont: [
+
+        ],
+        myOtherFont: [
+          "-apple-system",
+          "system-ui"
+        ]
+    },
 
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Тверской офис Accenture`,
+    // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+    copyright: `Copyright © ${new Date().getFullYear()} Тверской офис Accenture`,
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
+    highlight: {
+        // Highlight.js theme to use for syntax highlighting in code blocks.
+        theme: 'default',
+    },
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+    // Add custom scripts here that would be placed in <script> tags.
+    scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
+    // On page navigation for the current documentation page.
+    onPageNav: 'separate',
+    // No .html extensions for paths.
+    cleanUrl: true,
 
-  // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+    // Open Graph and Twitter card images.
+    ogImage: 'img/undraw_online.svg',
+    twitterImage: 'img/undraw_tweetstorm.svg',
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+    // Show documentation's last contributor's name.
+    // enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+    // Show documentation's last update time.
+    // enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+    // You may provide arbitrary config keys to be used as needed by your
+    // template. For example, if you need your repo's URL...
+    //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
